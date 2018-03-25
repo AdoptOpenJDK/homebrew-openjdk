0333973f29e8815ea058705081cc2acd8b5a074f
George Adams <george.adams@uk.ibm.com>
2018-03-25 22:07:35 +0100

    Update adoptopenjdk-openjdk8.rb

--- Formula/adoptopenjdk-openjdk8.rb
+++ Formula/adoptopenjdk-openjdk8.rb
@@ -13,15 +13,6 @@ class AdoptopenjdkOpenjdk8 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test adoptopenjdk-openjdk8`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system libexec/"bin/java", "-version"
   end
 end
