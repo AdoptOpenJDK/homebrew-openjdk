b1b91ddeb6c9ef703d0f349f833402e7262953b6
George Adams <george.adams@uk.ibm.com>
2018-03-25 23:31:53 +0100

    Update adoptopenjdk-openjdk9.rb

--- Formula/adoptopenjdk-openjdk9.rb
+++ Formula/adoptopenjdk-openjdk9.rb
@@ -5,6 +5,7 @@ class AdoptopenjdkOpenjdk9 < Formula
   desc "AdoptOpenJDK main binary releases for OpenJDK 9 with HotSpot"
   homepage "https://adoptopenjdk.net/"
   url "https://github.com/AdoptOpenJDK/openjdk9-releases/releases/download/jdk-9%2B181/OpenJDK9_x64_Mac_jdk-9.181.tar.gz"
+  version "jdk-9+181"
   sha256 "294fcddca3c0b4b1f4969030b7754fce284155b9d6d1cd233b2f928d44322c07"
 
   def install
