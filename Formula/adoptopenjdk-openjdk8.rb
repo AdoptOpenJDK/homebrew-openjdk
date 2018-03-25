c48c8d3fd29677f98c89710e36bd834020df88b1
George Adams <george.adams@uk.ibm.com>
2018-03-25 23:33:09 +0100

    Update adoptopenjdk-openjdk8.rb

--- Formula/adoptopenjdk-openjdk8.rb
+++ Formula/adoptopenjdk-openjdk8.rb
@@ -5,6 +5,7 @@ class AdoptopenjdkOpenjdk8 < Formula
   desc "AdoptOpenJDK main binary releases for OpenJDK 8 with HotSpot"
   homepage "https://adoptopenjdk.net/"
   url "https://github.com/AdoptOpenJDK/openjdk8-releases/releases/download/jdk8u144-b01/OpenJDK8_x64_Mac_jdk8u144-b01.tar.gz"
+  version "jdk8u144-b01"
   sha256 "dc0ae0cd1d87fa7b05cc7b4e3dcedcbb7f8f268118cdffb6d037dbd22ea82f13"
 
   def install
