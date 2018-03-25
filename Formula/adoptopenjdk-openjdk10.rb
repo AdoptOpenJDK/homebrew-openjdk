aa92b3ce6242210bba781bdabed5292a9f535b99
George Adams <george.adams@uk.ibm.com>
2018-03-25 23:34:03 +0100

    Update adoptopenjdk-openjdk10.rb

--- Formula/adoptopenjdk-openjdk10.rb
+++ Formula/adoptopenjdk-openjdk10.rb
@@ -5,6 +5,7 @@ class AdoptopenjdkOpenjdk10 < Formula
   desc "AdoptOpenJDK main binary releases for OpenJDK 10 with HotSpot"
   homepage "https://adoptopenjdk.net/"
   url "https://github.com/AdoptOpenJDK/openjdk10-releases/releases/download/jdk-10%2B23/OpenJDK10_x64_Mac_jdk-10.23.tar.gz"
+  version "jdk-10+23"
   sha256 "918b01c1fd7108064c6d45ed3fbea51396ca8b13bdc675bc550e4a53d70dd391"
 
   def install
