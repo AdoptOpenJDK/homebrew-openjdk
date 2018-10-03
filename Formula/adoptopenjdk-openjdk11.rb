c00b60a8f30e367ac7a0cfaadc521b4423f45a4e
Martijn Verburg <martijnverburg@gmail.com>
2018-10-03 19:50:15 +0100

    Fix 11 so it is a jdk and remove the PRD warning

--- Formula/adoptopenjdk-openjdk11.rb
+++ Formula/adoptopenjdk-openjdk11.rb
@@ -4,14 +4,13 @@
 class AdoptopenjdkOpenjdk11 < Formula
   desc "AdoptOpenJDK main binary releases for OpenJDK 11 with HotSpot"
   homepage "https://adoptopenjdk.net/"
-  url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11%2B28/OpenJDK11-jre_x64_mac_hotspot_11_28.tar.gz"
+  url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11%2B28/OpenJDK11-jdk_x64_mac_hotspot_11_28.tar.gz"
   version "jdk-11.28"
   sha256 "a7ef3669e22497e01fc47b28d7c66de42f0fee52b23302b75649dee239108812"
 
   def install
     prefix.install Dir["*"]
     ENV.prepend_path "PATH", Formula["adoptopenjdk-openjdk11"].libexec/"bin"
-    opoo "These binaries are not JCK compliant and should not be used in production"
   end
 
   test do
