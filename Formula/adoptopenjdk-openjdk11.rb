f6000753be6b04d8ced7c9c3f0d193a1e9350a61
Martijn Verburg <martijnverburg@gmail.com>
2018-10-06 11:01:16 +0100

    correct SHA

--- Formula/adoptopenjdk-openjdk11.rb
+++ Formula/adoptopenjdk-openjdk11.rb
@@ -7,6 +7,7 @@ class AdoptopenjdkOpenjdk11 < Formula
   url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11%2B28/OpenJDK11-jdk_x64_mac_hotspot_11_28.tar.gz"
   version "jdk-11.28"
   sha256 "e99866dfaf2b245a966b2d1e83a76b4f6f13727859c37ee248eb6c763951e536"
+
   def install
     prefix.install Dir["*"]
     ENV.prepend_path "PATH", Formula["adoptopenjdk-openjdk11"].libexec/"bin"
