238875440df03cbf983c9eda37bfbf1938f9b800
VonUniGE <VonUniGE@github.com>
2019-01-19 14:00:42 +0100

    Update adoptopenjdk8.rb to jdk8u202-b08

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk8' do
-  version '8,192:b12'
-  sha256 'cde59e884c473c3be52400bfad14b3ea1d8c42c994649f064ef335a727a36594'
+  version '8,202:b08'
+  sha256 '35e8f9b18f6c7b627dba13a4c6f45e6266552ccd7156043d92391443db0d60d6'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_colon}.tar.gz"
