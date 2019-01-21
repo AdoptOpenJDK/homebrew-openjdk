70283090aed7d11acb24672791d4a0f5d9ca1aac
VonUniGE <VonUniGE@github.com>
2019-01-21 17:51:08 +0100

    adoptopenjdk8.rb: fix style

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -10,8 +10,10 @@ cask 'adoptopenjdk8' do
 
   postflight do
     system_command '/bin/mv',
-                   args: ['-f', '--', "#{staged_path}/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}",
-                          "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"],
+                   args: [
+                           '-f', '--', "#{staged_path}/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}",
+                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
+                         ],
                    sudo: true
 
     system_command '/bin/mkdir',
@@ -19,8 +21,10 @@ cask 'adoptopenjdk8' do
                    sudo: true
 
     system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
-                          "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"],
+                   args: [
+                           '-nsf', '--', "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
+                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
+                         ],
                    sudo: true
 
     system_command '/usr/libexec/PlistBuddy',
