99d254f9a913adcfb1a14476f46d019ef153a352
George Adams <george.adams@uk.ibm.com>
2019-02-19 14:46:23 +0000

    Update adoptopenjdk8-openj9.rb

--- Casks/adoptopenjdk8-openj9.rb
+++ Casks/adoptopenjdk8-openj9.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk8-openj9' do
   version '8,202:b08'
-  sha256 '3f78d0f242c44d8bbdcd2719e8bbf7a1e8358e8e73e26ebd290cc277bd0fa96b'
+  sha256 '28ffc28154741c6be6dce8520ec91ccbb0b2af2e7d2d1b26305961e8d9dba58a'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}_openj9-0.12.1/OpenJDK#{version.before_comma}U-jdk_x64_mac_openj9_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_colon}_openj9-0.12.1.pkg"
