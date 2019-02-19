74f07ae30526184f88fccc7fe2f210f3004b18a4
George Adams <george.adams@uk.ibm.com>
2019-02-19 14:44:18 +0000

    add shasum

--- Casks/adoptopenjdk8-openj9.rb
+++ Casks/adoptopenjdk8-openj9.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk8-openj9' do
   version '8,202:b08'
-  sha256 :no_check
+  sha256 : '3f78d0f242c44d8bbdcd2719e8bbf7a1e8358e8e73e26ebd290cc277bd0fa96b'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}_openj9-0.12.1/OpenJDK#{version.before_comma}U-jdk_x64_mac_openj9_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_colon}_openj9-0.12.1.pkg"
