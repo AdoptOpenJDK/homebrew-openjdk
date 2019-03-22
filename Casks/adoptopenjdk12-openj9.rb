37e4f347ab88b66f63c340ffe4cde1483178bf2b
George Adams <georgeadams1995@gmail.com>
2019-03-22 12:46:01 +0000

    Update adoptopenjdk12-openj9.rb

--- Casks/adoptopenjdk12-openj9.rb
+++ Casks/adoptopenjdk12-openj9.rb
@@ -5,7 +5,7 @@ cask 'adoptopenjdk12-openj9' do
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk-#{version.before_comma}+#{version.after_comma.before_colon}_openj9-0.13.0/OpenJDK#{version.before_comma}U-jdk_x64_mac_openj9_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}_openj9-0.13.0.pkg"
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
-  name 'AdoptOpenJDK 12 (OpenJ9 JRE)'
+  name 'AdoptOpenJDK 12 (OpenJ9)'
   homepage 'https://adoptopenjdk.net/'
 
   pkg "OpenJDK#{version.before_comma}U-jdk_x64_mac_openj9_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}_openj9-0.13.0.pkg"
