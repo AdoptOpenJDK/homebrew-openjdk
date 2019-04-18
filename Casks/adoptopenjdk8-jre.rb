14d9bc5aae0c44ff6373b8d3c39dce4275dd17be
George Adams <georgeadams1995@gmail.com>
2019-04-18 12:10:27 +0100

    update adoptopenjdk8-jre to 8u212-b03

--- Casks/adoptopenjdk8-jre.rb
+++ Casks/adoptopenjdk8-jre.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk8-jre' do
-  version '8,202:b08'
-  sha256 '0c80cc5c18b3cedefa3e1bda9dda0fdd0ec5d3bc1d96ffbfc72d710ea60a49e5'
+  version '8,212:b03'
+  sha256 'e8c09bef25265dcd58756fd58137b9891b81e52692a60906c6b7ab801b868fe1'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jre_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_colon}.pkg"
+  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jre_x64_mac_hotspot_8u212b03.pkg'
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK 8'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg "OpenJDK#{version.before_comma}U-jre_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_colon}.pkg"
+  pkg 'jdk8u212-b03/OpenJDK8U-jre_x64_mac_hotspot_8u212b03.pkg'
 
   uninstall pkgutil: [
                        "net.adoptopenjdk.#{version.before_comma}.jre",
