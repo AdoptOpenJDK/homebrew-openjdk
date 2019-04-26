019d5296e2f99dffb763edd511966692f114b86a
George Adams <george@jclarity.com>
2019-04-26 15:13:27 +0100

    update openjdk12-hotspot-jdk to 12.0.1+12 (#115)

--- Casks/adoptopenjdk12.rb
+++ Casks/adoptopenjdk12.rb
@@ -1,16 +1,14 @@
 cask 'adoptopenjdk12' do
-  version '12,33'
-  sha256 '3b974189c4eb74aa43a365676905f6534faf599075e3910fa6ec3b8ffb101cbf'
+  version '12.0.1+12'
+  sha256 'db0e39ee1e747e436d41a110f037677540b2bc3f086b4fe4eaaca9f5c2de48ad'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk-#{version.before_comma}+#{version.after_comma.before_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}.pkg"
-  appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
+  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.1%2B12/OpenJDK12U-jdk_x64_mac_hotspot_12.0.1_12.pkg'
+  appcast 'https://github.com/adoptopenjdk/openjdk12-binaries/releases.atom'
   name 'AdoptOpenJDK 12'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg "OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}.pkg"
+  pkg 'OpenJDK12U-jdk_x64_mac_hotspot_12.0.1_12.pkg'
 
-  uninstall pkgutil: [
-                       "net.adoptopenjdk.#{version.before_comma}.jdk",
-                     ]
+  uninstall pkgutil: 'net.adoptopenjdk.12.jdk'
 end
