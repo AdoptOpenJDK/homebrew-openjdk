509788a33677eb7b2daa716f5b1914dd6671a7e5
George Adams <george@jclarity.com>
2019-04-26 15:14:43 +0100

    update openjdk12-openj9-jre to 12.0.1+12 (#114)
    * update openjdk12-openj9-jre to 12.0.1+12
    
    * Update adoptopenjdk12-openj9-jre.rb

--- Casks/adoptopenjdk12-openj9-jre.rb
+++ Casks/adoptopenjdk12-openj9-jre.rb
@@ -1,16 +1,14 @@
 cask 'adoptopenjdk12-openj9-jre' do
-  version '12,33'
-  sha256 '2ad5f8c73a32ac78a00ce197eb8fe97efa57a6889fe2fcb762a419af8e722147'
+  version '12.0.1+12'
+  sha256 '8a2cdfc418cac8b44cff3ec787ec5ab5dbdbf5593aec9c837135e1a605ddadef'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk-#{version.before_comma}+#{version.after_comma.before_colon}_openj9-0.13.0/OpenJDK#{version.before_comma}U-jre_x64_mac_openj9_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}_openj9-0.13.0.pkg"
-  appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
+  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.1%2B12_openj9-0.14.1/OpenJDK12U-jre_x64_mac_openj9_12.0.1_12_openj9-0.14.1.pkg'
+  appcast 'https://github.com/adoptopenjdk/openjdk12-binaries/releases.atom'
   name 'AdoptOpenJDK 12 (OpenJ9 JRE)'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg "OpenJDK#{version.before_comma}U-jre_x64_mac_openj9_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}_openj9-0.13.0.pkg"
+  pkg 'OpenJDK12U-jre_x64_mac_openj9_12.0.1_12_openj9-0.14.1.pkg'
 
-  uninstall pkgutil: [
-                       "net.adoptopenjdk.#{version.before_comma}-openj9.jre",
-                     ]
+  uninstall pkgutil: 'net.adoptopenjdk.12-openj9.jre'
 end
