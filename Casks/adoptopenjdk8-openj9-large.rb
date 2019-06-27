86f00aec95eac3cc82aff3e212130c21a8511f11
Majki <majki@majki.org>
2019-06-27 12:57:44 +0200

    update adoptopenjdk8-openj9-large to 8u212b04 (#124)

--- Casks/adoptopenjdk8-openj9-large.rb
+++ Casks/adoptopenjdk8-openj9-large.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk8-openj9-large' do
-  version '8,212:b03'
-  sha256 '0c6e861d87abdb8336e9b77076f660bf6306e2553f4aa653e769d3078fbf826a'
+  version '8,212:b04'
+  sha256 'a0f82d276af4c7419b681330e24a82a53b32a9cc0b8473e7ca5350df0275248e'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03_openj9-0.14.0/OpenJDK8U-jdk_x64_mac_openj9_macosXL_8u212b03_openj9-0.14.0.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04_openj9-0.14.2/OpenJDK8U-jdk_x64_mac_openj9_macosXL_8u212b04_openj9-0.14.2.pkg'
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK 8'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK8U-jdk_x64_mac_openj9_macosXL_8u212b03_openj9-0.14.0.pkg'
+  pkg 'OpenJDK8U-jdk_x64_mac_openj9_macosXL_8u212b04_openj9-0.14.2.pkg'
 
   uninstall pkgutil: [
                        "net.adoptopenjdk.#{version.before_comma}-openj9.jdk",
