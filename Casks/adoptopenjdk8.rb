e1139b0135114b59363c4422deede120b8193d54
George Adams <george@jclarity.com>
2019-07-19 10:21:35 +0100

    Update adoptopenjdk8.rb

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk8' do
   version '8,222:b10'
-  sha256 '9605fd00d2960934422437f601c7a9a1c5537309b9199d5bc75f84f20cd29a76'
+  sha256 '21b4e428746cd7b930f4ffb5de53850a2a8e9a08bb53346b6e81567639473603'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u222b10.pkg'
