231eaf2f46ce877f06e84872ac09b85104a68669
George Adams <georgeadams1995@gmail.com>
2019-07-19 10:23:02 +0100

    update 8-j9-jdk

--- Casks/adoptopenjdk8-openj9.rb
+++ Casks/adoptopenjdk8-openj9.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk8-openj9' do
-  version '8,212:b04'
-  sha256 '93efa8c103164418cf37c4ae4fa231f5d2541cf73f5d08abe754ecc9627fc237'
+  version '8,222:b10'
+  sha256 '6b6c1acf1edef36e6d09915b1fb9e5291b17ef15f703706b067665443b8e1b90'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04_openj9-0.14.2/OpenJDK8U-jdk_x64_mac_openj9_8u212b04_openj9-0.14.2.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10_openj9-0.15.1/OpenJDK8U-jdk_x64_mac_openj9_8u222b10_openj9-0.15.1.pkg'
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK 8'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK8U-jdk_x64_mac_openj9_8u212b04_openj9-0.14.2.pkg'
+  pkg 'OpenJDK8U-jdk_x64_mac_openj9_8u222b10_openj9-0.15.1.pkg'
 
   uninstall pkgutil: [
                        "net.adoptopenjdk.#{version.before_comma}-openj9.jdk",
