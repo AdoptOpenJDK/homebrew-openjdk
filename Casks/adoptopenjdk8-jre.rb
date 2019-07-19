9f5a09498e246a968b8f98c94dc4b28ea1087307
George Adams <georgeadams1995@gmail.com>
2019-07-19 10:17:01 +0100

    update 8-hspt-jre

--- Casks/adoptopenjdk8-jre.rb
+++ Casks/adoptopenjdk8-jre.rb
@@ -1,9 +1,9 @@
 cask 'adoptopenjdk8-jre' do
-  version '8,212:b04'
-  sha256 '6d0a03c4aef15a7bf1889a029c63a74391cc835b4b1bb46136029b9cbe57a033'
+  version '8,222:b10'
+  sha256 'b3ac2436534cea932ccf665b317dbf5ffc0ee065efca808b22b6c2d795ca1b90'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04/OpenJDK8U-jre_x64_mac_hotspot_8u212b04.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10/OpenJDK8U-jre_x64_mac_hotspot_8u222b10.pkg'
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK 8'
   homepage 'https://adoptopenjdk.net/'
