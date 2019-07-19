89295950b2d87015fff1ab660de59425b22a4c52
George Adams <georgeadams1995@gmail.com>
2019-07-19 10:33:34 +0100

    update 11-htspt-jdk

--- Casks/adoptopenjdk11.rb
+++ Casks/adoptopenjdk11.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk11' do
-  version '11,0.3:7'
-  sha256 '8e8e4373b59828293f2035c4cb4bb2758a26adc6b30ab28f383248e04983f9a7'
+  version '11,0.4:11'
+  sha256 '28c3e4d85ea09b8a5428ab5c5a43da25d777b256748b8c12453627ff91838c05'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11/OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.pkg'
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK 11'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.pkg'
+  pkg 'OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.pkg'
 
   uninstall pkgutil: "net.adoptopenjdk.#{version.before_comma}.jdk"
 end
