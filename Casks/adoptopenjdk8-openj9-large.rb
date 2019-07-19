2f59b6f2205b728b5febeefcaf4b806bd86279ba
George Adams <george@jclarity.com>
2019-07-19 10:49:44 +0100

    Update adoptopenjdk8-openj9-large.rb

--- Casks/adoptopenjdk8-openj9-large.rb
+++ Casks/adoptopenjdk8-openj9-large.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk8-openj9-large' do
   version '8,222:b10'
-  sha256 '6b6c1acf1edef36e6d09915b1fb9e5291b17ef15f703706b067665443b8e1b90'
+  sha256 '929da901d7a5a2abf243ed9dc5a0e540d89fb28fe80cc1d50c2ef135148b69db'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10_openj9-0.15.1/OpenJDK8U-jdk_x64_mac_openj9_macosXL_8u222b10_openj9-0.15.1.pkg'
