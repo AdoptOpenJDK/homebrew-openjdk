b4ce87108849d2af08f6ae1ba876c1f8c48e34e8
Majki <majki@majki.org>
2019-07-21 11:10:32 +0200

    update adoptopenjdk12 to 12.0.2-b10

--- Casks/adoptopenjdk12.rb
+++ Casks/adoptopenjdk12.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk12' do
-  version '12.0.1+12'
-  sha256 'db0e39ee1e747e436d41a110f037677540b2bc3f086b4fe4eaaca9f5c2de48ad'
+  version '12.0.2+10'
+  sha256 '2cff36b04343762fe323f1bbbc2ca08c8116e6e8009fb166c30d57bf0bda1c82'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.1%2B12/OpenJDK12U-jdk_x64_mac_hotspot_12.0.1_12.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10/OpenJDK12U-jdk_x64_mac_hotspot_12.0.2_10.pkg'
   appcast 'https://github.com/adoptopenjdk/openjdk12-binaries/releases.atom'
   name 'AdoptOpenJDK 12'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK12U-jdk_x64_mac_hotspot_12.0.1_12.pkg'
+  pkg 'OpenJDK12U-jdk_x64_mac_hotspot_12.0.2_10.pkg'
 
   uninstall pkgutil: 'net.adoptopenjdk.12.jdk'
 end
