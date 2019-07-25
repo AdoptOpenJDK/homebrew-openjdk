0b289ccc689cc82c19fd4b3c12769ee887e369ae
Majki <majki@majki.org>
2019-07-25 16:09:36 +0200

    update adoptopenjdk12-openj9 to 12.0.2-b10 (#143)

--- Casks/adoptopenjdk12-openj9.rb
+++ Casks/adoptopenjdk12-openj9.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk12-openj9' do
-  version '12.0.1+12'
-  sha256 '2b8ec4869cb9aad08ab6fe8e7e20a1877b547ef594db4968cde2dcd0adfde2a7'
+  version '12.0.2+10'
+  sha256 '71f0d8ae7bbb398efa345e807440a22add33cb131b52610050ca4d72d338a879'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.1%2B12_openj9-0.14.1/OpenJDK12U-jdk_x64_mac_openj9_12.0.1_12_openj9-0.14.1.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10_openj9-0.15.1/OpenJDK12U-jdk_x64_mac_openj9_12.0.2_10_openj9-0.15.1.pkg'
   appcast 'https://github.com/adoptopenjdk/openjdk12-binaries/releases.atom'
   name 'AdoptOpenJDK 12 (OpenJ9)'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK12U-jdk_x64_mac_openj9_12.0.1_12_openj9-0.14.1.pkg'
+  pkg 'OpenJDK12U-jdk_x64_mac_openj9_12.0.2_10_openj9-0.15.1.pkg'
 
   uninstall pkgutil: 'net.adoptopenjdk.12-openj9.jdk'
 end
