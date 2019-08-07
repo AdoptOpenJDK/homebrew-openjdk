fbaca1f7e9872766cf49cde30f6e8b171ddd08af
George Adams <georgeadams1995@gmail.com>
2019-08-07 12:59:03 +0100

    update adoptopenjdk12.rb to 12.0.2,10

--- Casks/adoptopenjdk12.rb
+++ Casks/adoptopenjdk12.rb
@@ -1,9 +1,9 @@
 cask 'adoptopenjdk12' do
   version '12.0.2,10'
-  sha256 '2cff36b04343762fe323f1bbbc2ca08c8116e6e8009fb166c30d57bf0bda1c82'
+  sha256 '146c83e02e3ea058f93470c8e3b32bfa01e288175723e615c60a993e98b159fc'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10/OpenJDK12U-jdk_x64_mac_hotspot_12.0.2_10.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10.2/OpenJDK12U-jdk_x64_mac_hotspot_12.0.2_10.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK 12'
   homepage 'https://adoptopenjdk.net/'
