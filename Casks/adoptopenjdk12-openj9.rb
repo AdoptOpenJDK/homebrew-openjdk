ce862cd279eae714a3ba989e75d3b71030518cd5
George Adams <george@jclarity.com>
2019-08-07 13:18:01 +0100

    Update adoptopenjdk12-openj9.rb

--- Casks/adoptopenjdk12-openj9.rb
+++ Casks/adoptopenjdk12-openj9.rb
@@ -10,5 +10,5 @@ cask 'adoptopenjdk12-openj9' do
 
   pkg 'OpenJDK12U-jdk_x64_mac_openj9_12.0.2_10_openj9-0.15.1.pkg'
 
-  uninstall pkgutil: 'net.adoptopenjdk.#{version.major}-openj9.jdk'
+  uninstall pkgutil: "net.adoptopenjdk.#{version.major}-openj9.jdk"
 end
