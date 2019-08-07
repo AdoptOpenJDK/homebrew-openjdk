da7ed854915298f0172a87849e67a63b5c288b83
George Adams <georgeadams1995@gmail.com>
2019-08-07 10:41:06 +0100

    update 11-htspt-jdk

--- Casks/adoptopenjdk11.rb
+++ Casks/adoptopenjdk11.rb
@@ -1,9 +1,9 @@
 cask 'adoptopenjdk11' do
   version '11.0.4,11'
-  sha256 '28c3e4d85ea09b8a5428ab5c5a43da25d777b256748b8c12453627ff91838c05'
+  sha256 '23b8776689cb6d2b8073f331147b2ea9a52097d28d256ae03734e1ac2c7cfa40'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11/OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11.2/OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK 11'
   homepage 'https://adoptopenjdk.net/'
