3343c230e4faad681e7bdd0264231b91d6fb4288
Jack Steel <mail@jacksteel.co.nz>
2019-08-25 23:24:11 +1200

    update adoptopenjdk11-openj9-jre.rb to 11.0.4,11

--- Casks/adoptopenjdk11-openj9-jre.rb
+++ Casks/adoptopenjdk11-openj9-jre.rb
@@ -1,9 +1,9 @@
 cask 'adoptopenjdk11-openj9-jre' do
   version '11.0.4,11'
-  sha256 '8595fcf58f0894d105d629180656425915154dc0b3737e89fca9d8984bf6aedf'
+  sha256 'b62497189f9714016c882b627fa2bbc4be119be4406a1a463de5ab5d052b3796'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11.2_openj9-0.15.1/OpenJDK11U-jre_x64_mac_openj9_11.0.4_11_openj9-0.15.1.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11.4_openj9-0.15.1/OpenJDK11U-jre_x64_mac_openj9_11.0.4_11_openj9-0.15.1.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK 11 (OpenJ9 JRE)'
   homepage 'https://adoptopenjdk.net/'
