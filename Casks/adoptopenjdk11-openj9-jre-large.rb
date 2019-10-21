d075c57497bfa72d6a3004dc4b621093698f24bf
George Adams <georgeadams1995@gmail.com>
2019-10-21 12:37:14 +0100

    update adoptopenjdk11-openj9-jre-large.rb to 11.0.5,10 (#181)

--- Casks/adoptopenjdk11-openj9-jre-large.rb
+++ Casks/adoptopenjdk11-openj9-jre-large.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk11-openj9-jre-large' do
-  version '11.0.4,11'
-  sha256 'f5ae4105d5c7408be4d93486cdd2061144f43b33f98fe9ce3b32711451915a45'
+  version '11.0.5,10'
+  sha256 '92626fd2805289f50389f67e5ccf6122bb9a159877158334cec6db867d1a0147'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11.4_openj9-0.15.1/OpenJDK11U-jre_x64_mac_openj9_macosXL_11.0.4_11_openj9-0.15.1.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10_openj9-0.17.0/OpenJDK11U-jre_x64_mac_openj9_macosXL_11.0.5_10_openj9-0.17.0.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK 11 (OpenJ9 JRE) (XL)'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK11U-jre_x64_mac_openj9_macosXL_11.0.4_11_openj9-0.15.1.pkg'
+  pkg 'OpenJDK11U-jre_x64_mac_openj9_macosXL_11.0.5_10_openj9-0.17.0.pkg'
 
   uninstall pkgutil: "net.adoptopenjdk.#{version.major}-openj9.jre"
 end
