1e4eeb5b672051ee7c915c03e56078f85a04fecf
George Adams <georgeadams1995@gmail.com>
2019-10-21 11:29:43 +0100

    update adoptopenjdk11.rb to 11.0.5,10

--- Casks/adoptopenjdk11.rb
+++ Casks/adoptopenjdk11.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk11' do
-  version '11.0.4,11'
-  sha256 '555fd51cb60aa37967db48ee1bd6ec184dd2c2fbf045493f16203c1ca4494359'
+  version '11.0.5,10'
+  sha256 '35840dce2f1fc20613b1866740a8ead039c0a44620d84446c7afc874a56ca917'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11.4/OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/11.0.5%2B10/OpenJDK11U-jdk_x64_mac_hotspot_11.0.5_10.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK 11'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.pkg'
+  pkg 'OpenJDK11U-jdk_x64_mac_hotspot_11.0.5_10.pkg'
 
   uninstall pkgutil: "net.adoptopenjdk.#{version.major}.jdk"
 end
