7119550af8699aaca134993ffed09a06ce2334ff
George Adams <georgeadams1995@gmail.com>
2019-10-21 13:03:07 +0100

    update adoptopenjdk11.rb to 11.0.5,10 (#198)

--- Casks/adoptopenjdk11.rb
+++ Casks/adoptopenjdk11.rb
@@ -3,7 +3,7 @@ cask 'adoptopenjdk11' do
   sha256 '35840dce2f1fc20613b1866740a8ead039c0a44620d84446c7afc874a56ca917'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/11.0.5%2B10/OpenJDK11U-jdk_x64_mac_hotspot_11.0.5_10.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jdk_x64_mac_hotspot_11.0.5_10.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK 11'
   homepage 'https://adoptopenjdk.net/'
