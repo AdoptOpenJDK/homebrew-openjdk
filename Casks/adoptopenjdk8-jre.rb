5c91ee51bae92355d8d0f252a37c378e26499dc4
George Adams <georgeadams1995@gmail.com>
2019-10-31 10:36:34 +0000

    update adoptopenjdk8-jre.rb to 8,"8.0.232+9":"8.0.232+9"

--- Casks/adoptopenjdk8-jre.rb
+++ Casks/adoptopenjdk8-jre.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk8-jre' do
-  version '8,222:b10'
-  sha256 'a67ff13b3447b261b304972486cf84149745fb466d88b46253f12878be68605e'
+  version '8,"8.0.232+9":"8.0.232+9"'
+  sha256 'c4219c01085a84bb5a3d875eb438335ab68e3fe7af362a4121ad44d609ce7b43'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10/OpenJDK8U-jre_x64_mac_hotspot_8u222b10.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jre_x64_mac_hotspot_8u232b09.pkg'
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases/latest"
   name 'AdoptOpenJDK 8'
   homepage 'https://adoptopenjdk.net/'
 
-  pkg 'OpenJDK8U-jre_x64_mac_hotspot_8u222b10.pkg'
+  pkg 'OpenJDK8U-jre_x64_mac_hotspot_8u232b09.pkg'
 
   uninstall pkgutil: [
                        "net.adoptopenjdk.#{version.before_comma}.jre",
