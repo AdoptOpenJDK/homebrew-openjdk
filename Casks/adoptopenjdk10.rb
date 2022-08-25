c9d9fe1cc1447d04c055d2c5831754a9d4e15289
Wen Zhou <wenzhou@redhat.com>
2022-08-25 11:33:47 +0200

    update: change after_comma before_colon to csv
    	- jdk11 build require bootjdk10 on macos
    	- must use version variable in url

--- Casks/adoptopenjdk10.rb
+++ Casks/adoptopenjdk10.rb
@@ -3,45 +3,45 @@ cask "adoptopenjdk10" do
   sha256 "550fac840563073dcfaa1ffd4a3a9cc68fd8ccc71bbc65baf3cb32ec734c30dd"
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk10-binaries/releases/download/jdk-10.0.2%2B13.1/OpenJDK10U-jdk_x64_mac_hotspot_10.0.2_13.tar.gz"
-  appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-10.0.2%2B13.1/OpenJDK10U-jdk_x64_mac_hotspot_10.0.2_13.tar.gz", verified: "https://github.com/AdoptOpenJDK"
+  appcast "https://github.com/AdoptOpenJDK/openjdk10-binaries/releases/latest"
   name "AdoptOpenJDK 10"
   desc "AdoptOpenJDK OpenJDK (Java) Development Kit"
   homepage "https://adoptopenjdk.net/"
 
   postflight do
     system_command "/bin/mv",
-                   args: ["-f", "--", "#{staged_path}/jdk-#{version.before_comma}.#{version.after_comma.before_colon}+#{version.after_colon}", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"],
+                   args: ["-f", "--", "#{staged_path}/jdk-10.0.2+13", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk"],
                    sudo: true
 
     system_command "/bin/mkdir",
-                   args: ["-p", "--", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
+                   args: ["-p", "--", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Home/bundle/Libraries"],
                    sudo: true
 
     system_command "/bin/ln",
-                   args: ["-nsf", "--", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"],
+                   args: ["-nsf", "--", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Home/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Home/bundle/Libraries/libserver.dylib"],
                    sudo: true
 
     system_command "/usr/libexec/PlistBuddy",
-                   args: ["-c", "Set :CFBundleGetInfoString AdoptOpenJDK #{version.before_comma}+#{version.after_comma}", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"],
+                   args: ["-c", "Set :CFBundleGetInfoString AdoptOpenJDK 10+0.2:13", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Info.plist"],
                    sudo: true
 
     system_command "/usr/libexec/PlistBuddy",
-                   args: ["-c", "Set :CFBundleIdentifier net.adoptopenjdk.#{version.before_comma}.jdk", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"],
+                   args: ["-c", "Set :CFBundleIdentifier net.adoptopenjdk.10.jdk", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Info.plist"],
                    sudo: true
 
     system_command "/usr/libexec/PlistBuddy",
-                   args: ["-c", "Set :CFBundleName AdoptOpenJDK #{version.before_comma}", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"],
+                   args: ["-c", "Set :CFBundleName AdoptOpenJDK 10", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Info.plist"],
                    sudo: true
 
     system_command "/usr/libexec/PlistBuddy",
-                   args: ["-c", "Set :JavaVM:JVMPlatformVersion #{version.before_comma}.#{version.after_comma}", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"],
+                   args: ["-c", "Set :JavaVM:JVMPlatformVersion 10.0.2:13", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Info.plist"],
                    sudo: true
 
     system_command "/usr/libexec/PlistBuddy",
-                   args: ["-c", "Set :JavaVM:JVMVendor AdoptOpenJDK", "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"],
+                   args: ["-c", "Set :JavaVM:JVMVendor AdoptOpenJDK", "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk/Contents/Info.plist"],
                    sudo: true
   end
 
-  uninstall delete: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
+  uninstall delete: "/Library/Java/JavaVirtualMachines/adoptopenjdk-10.jdk"
 end
